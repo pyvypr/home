78b1847a4baca90663a5568331b9370988ef627a
Marta Han <han.marta@gmail.com>
2020-07-10 14:55:01 +0200

    removed duplicate slideshow functions and commented out the  link to test cases

--- static/js/analysis.js
+++ static/js/analysis.js
@@ -1,15 +1,8 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
-}
-
-function plusSlides2(n) {
-  showSlides2(slideIndex2 += n);
 }
 
 function currentSlide(n) {
@@ -25,15 +18,4 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-}
-
-function showSlides2(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides2");
-  if (n > slides.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex2-1].style.display = "block";
 }
