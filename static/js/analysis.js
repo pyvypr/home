a15eacdcebcc404c106ea0ac7015a028242c76b6
Marta Han <han.marta@gmail.com>
2020-07-20 15:22:01 +0200

    added some more screenshots because of style change, adding path highlighting

--- static/js/analysis.js
+++ static/js/analysis.js
@@ -1,21 +1,33 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [];
+showSlides(1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, slideshowIndex = 0) {
+  slideIndex[slideshowIndex] += n
+  showSlides(slideIndex[slideshowIndex], slideshowIndex);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+function showSlides(n, slideshowIndex = null) {
+  var i, j;
+  var slideshows = $(".slideshow-container");
+
+  if (slideshowIndex != null) {
+    var slides = $(slideshows[slideshowIndex]).find(".mySlides");
+    if (n > slides.length) {slideIndex[slideshowIndex] = 1}
+    if (n < 1) {slideIndex[slideshowIndex] = slides.length}
+    for (j = 0; j < slides.length; j++) {
+        $(slides[j]).attr('style', 'display: none');
+    }
+    $(slides[slideIndex[slideshowIndex]-1]).attr('style', 'display: block');
+  } else {
+    for (i = 0; i < slideshows.length; i++) {
+      slideIndex.push(n);
+      var slides = $(slideshows[i]).find(".mySlides");
+      for (j = 0; j < slides.length; j++) {
+          $(slides[j]).attr('style', 'display: none');
+      }
+      $(slides[0]).attr('style', 'display: block');
+    }
   }
-  slides[slideIndex-1].style.display = "block";
+
 }
